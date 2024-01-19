89b78ff77fd4e93f4511b2e954a543da0d2499dc
Marcelo Gonçalves <juninho_toh@hotmail.com>
2024-01-19 09:56:27 -0300

    feat: adicionando identificação de nome por socket.id

--- 023-chat-com-websocket_v1/src/server/socket-back.js
+++ 023-chat-com-websocket_v1/src/server/socket-back.js
@@ -1,9 +1,19 @@
 const io = require("./server.js");
 
+let clients = {};
+
 io.on("connection", (socket) => {
     // console.log("Usuário conectado: " + socket.id);
+    clients[socket.id] = {name: `Anônimo${parseInt(Math.random() * 1000 + 1)}`}
 
     socket.on("message", (texto) => {
-        io.emit("message", texto);
+        io.emit("message", 
+            `${clients[socket.id].name}: ${texto}`,
+        );
+    });
+
+    socket.on("disconnect", () => {
+        console.log(`Usuário ${clients[socket.id].name} disconnected`);
+        delete clients[socket.id];
     });
 });
